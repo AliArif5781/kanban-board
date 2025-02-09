1af7faf5a41179f7e8db4018e1d1d1062b8a2047
AliArif5781 <aliairf92@gmail.com>
2025-02-09 19:51:43 +0500

    SortableItem.tsx

--- src/components/common/SortableItem.tsx
+++ src/components/common/SortableItem.tsx
@@ -1,36 +1,26 @@
-import React from "react";
+// SortableItem.tsx
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-interface SortableItemProps {
-  id: string;
-  children: React.ReactNode;
-}
-
-export const SortableItem = ({ id, children }: SortableItemProps) => {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({ id });
+export const SortableItem = ({ id, index, children }: any) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id,
+      data: {
+        type: "task",
+        sortable: {
+          index,
+        },
+      },
+    });
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
-    opacity: isDragging ? 0.5 : 1,
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className=" touch-none"
-    >
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
       {children}
     </div>
   );
